636c66816085ba041ed618aa0e6e1c9c3003393d
Baby055 <hei.sombiniaina@gmail.com>
2026-06-08 11:55:19 +0300

    Book.java : correction

--- src/main/java/com/hei/prog/entity/Book.java
+++ src/main/java/com/hei/prog/entity/Book.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 public class Book {
   private String id;
   private String title;
-  private String author;
+  private Author author;
   private List<BookCopy> bookCopyList;
   private Category category;
 }
