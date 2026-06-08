831157980ee9fe5b66152da29c0bf058d71b247d
Baby055 <hei.sombiniaina@gmail.com>
2026-06-08 19:26:24 +0300

    repository/AuthorRepository.java : initialisation

---
+++ src/main/java/com/hei/prog/repository/AuthorRepository.java
@@ -0,0 +1,8 @@
+package com.hei.prog.repository;
+
+import com.hei.prog.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, String> {}
