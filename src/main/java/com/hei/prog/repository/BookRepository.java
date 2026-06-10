c98cf57a46bf0c85f6031ef8060d55f440501e30
Baby055 <hei.sombiniaina@gmail.com>
2026-06-10 20:15:14 +0300

    src/repository/BookRepository.java : Enrichissement

--- src/main/java/com/hei/prog/repository/BookRepository.java
+++ src/main/java/com/hei/prog/repository/BookRepository.java
@@ -1,9 +1,20 @@
 package com.hei.prog.repository;
 
 import com.hei.prog.entity.Book;
+import com.hei.prog.entity.enums.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, String> {
+    List<Book> findByTitle(String title);
+    List<Book> findByTitleContainingIgnoreCase(String titlePart);
+    List<Book> findByAuthor(String author);
+    List<Book> findByAuthorContainingIgnoreCase(String authorPart);
+    List<Book> findByCategory(Category category);
+    List<Book> findByCategoryAndTitleContainingIgnoreCase(Category category, String titlePart);
+    List<Book> findByAuthorAndCategory(String author, Category category);
+    long countByCategory(Category category);
 }
