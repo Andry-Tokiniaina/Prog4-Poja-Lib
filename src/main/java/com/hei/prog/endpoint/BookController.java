6e04039c2b0bb13edca3e6c3f688c50e0895b9a5
Baby055 <hei.sombiniaina@gmail.com>
2026-06-11 08:40:13 +0300

    formaté

--- src/main/java/com/hei/prog/endpoint/BookController.java
+++ src/main/java/com/hei/prog/endpoint/BookController.java
@@ -3,80 +3,80 @@ package com.hei.prog.endpoint;
 import com.hei.prog.entity.Book;
 import com.hei.prog.entity.enums.Category;
 import com.hei.prog.services.BookService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
 
-    @Autowired
-    private BookService bookService;
+  @Autowired private BookService bookService;
 
-    @GetMapping
-    public List<Book> getAllBooks() {
-        return bookService.getAllBooks();
-    }
+  @GetMapping
+  public List<Book> getAllBooks() {
+    return bookService.getAllBooks();
+  }
 
-    @GetMapping("/{id}")
-    public Book getBookById(@PathVariable String id) {
-        return bookService.getBookById(id);
-    }
+  @GetMapping("/{id}")
+  public Book getBookById(@PathVariable String id) {
+    return bookService.getBookById(id);
+  }
 
-    @PostMapping
-    public Book createBook(@RequestBody Book book) {
-        return bookService.createBook(book);
-    }
+  @PostMapping
+  public Book createBook(@RequestBody Book book) {
+    return bookService.createBook(book);
+  }
 
-    @PutMapping("/{id}")
-    public Book updateBook(@PathVariable String id, @RequestBody Book book) {
-        return bookService.updateBook(id, book);
-    }
+  @PutMapping("/{id}")
+  public Book updateBook(@PathVariable String id, @RequestBody Book book) {
+    return bookService.updateBook(id, book);
+  }
 
-    @DeleteMapping("/{id}")
-    public void deleteBook(@PathVariable String id) {
-        bookService.deleteBook(id);
-    }
+  @DeleteMapping("/{id}")
+  public void deleteBook(@PathVariable String id) {
+    bookService.deleteBook(id);
+  }
 
-    @GetMapping("/search/title")
-    public List<Book> searchByTitle(@RequestParam String title) {
-        return bookService.getBooksByTitle(title);
-    }
+  @GetMapping("/search/title")
+  public List<Book> searchByTitle(@RequestParam String title) {
+    return bookService.getBooksByTitle(title);
+  }
 
-    @GetMapping("/search/title-contains")
-    public List<Book> searchByTitleContaining(@RequestParam String keyword) {
-        return bookService.getBooksByTitleContaining(keyword);
-    }
+  @GetMapping("/search/title-contains")
+  public List<Book> searchByTitleContaining(@RequestParam String keyword) {
+    return bookService.getBooksByTitleContaining(keyword);
+  }
 
-    @GetMapping("/search/author")
-    public List<Book> searchByAuthor(@RequestParam String author) {
-        return bookService.getBooksByAuthor(author);
-    }
+  @GetMapping("/search/author")
+  public List<Book> searchByAuthor(@RequestParam String author) {
+    return bookService.getBooksByAuthor(author);
+  }
 
-    @GetMapping("/search/author-contains")
-    public List<Book> searchByAuthorContaining(@RequestParam String keyword) {
-        return bookService.getBooksByAuthorContaining(keyword);
-    }
+  @GetMapping("/search/author-contains")
+  public List<Book> searchByAuthorContaining(@RequestParam String keyword) {
+    return bookService.getBooksByAuthorContaining(keyword);
+  }
 
-    @GetMapping("/search/category")
-    public List<Book> searchByCategory(@RequestParam Category category) {
-        return bookService.getBooksByCategory(category);
-    }
+  @GetMapping("/search/category")
+  public List<Book> searchByCategory(@RequestParam Category category) {
+    return bookService.getBooksByCategory(category);
+  }
 
-    @GetMapping("/search/category-title")
-    public List<Book> searchByCategoryAndTitle(@RequestParam Category category, @RequestParam String keyword) {
-        return bookService.getBooksByCategoryAndTitle(category, keyword);
-    }
+  @GetMapping("/search/category-title")
+  public List<Book> searchByCategoryAndTitle(
+      @RequestParam Category category, @RequestParam String keyword) {
+    return bookService.getBooksByCategoryAndTitle(category, keyword);
+  }
 
-    @GetMapping("/search/author-category")
-    public List<Book> searchByAuthorAndCategory(@RequestParam String author, @RequestParam Category category) {
-        return bookService.getBooksByAuthorAndCategory(author, category);
-    }
+  @GetMapping("/search/author-category")
+  public List<Book> searchByAuthorAndCategory(
+      @RequestParam String author, @RequestParam Category category) {
+    return bookService.getBooksByAuthorAndCategory(author, category);
+  }
 
-    @GetMapping("/count/category")
-    public long countByCategory(@RequestParam Category category) {
-        return bookService.countBooksByCategory(category);
-    }
+  @GetMapping("/count/category")
+  public long countByCategory(@RequestParam Category category) {
+    return bookService.countBooksByCategory(category);
+  }
 }
--- src/main/java/com/hei/prog/repository/BookRepository.java
+++ src/main/java/com/hei/prog/repository/BookRepository.java
@@ -2,19 +2,25 @@ package com.hei.prog.repository;
 
 import com.hei.prog.entity.Book;
 import com.hei.prog.entity.enums.Category;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface BookRepository extends JpaRepository<Book, String> {
-    List<Book> findByTitle(String title);
-    List<Book> findByTitleContainingIgnoreCase(String titlePart);
-    List<Book> findByAuthor(String author);
-    List<Book> findByAuthorContainingIgnoreCase(String authorPart);
-    List<Book> findByCategory(Category category);
-    List<Book> findByCategoryAndTitleContainingIgnoreCase(Category category, String titlePart);
-    List<Book> findByAuthorAndCategory(String author, Category category);
-    long countByCategory(Category category);
+  List<Book> findByTitle(String title);
+
+  List<Book> findByTitleContainingIgnoreCase(String titlePart);
+
+  List<Book> findByAuthor(String author);
+
+  List<Book> findByAuthorContainingIgnoreCase(String authorPart);
+
+  List<Book> findByCategory(Category category);
+
+  List<Book> findByCategoryAndTitleContainingIgnoreCase(Category category, String titlePart);
+
+  List<Book> findByAuthorAndCategory(String author, Category category);
+
+  long countByCategory(Category category);
 }
